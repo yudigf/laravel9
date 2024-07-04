087d130ce897392c6a85c054e2c1833f19c48b5f
yudigf08 <yudi.fitory@wgs-id.com>
2024-07-04 14:56:00 +0700

    membuat Login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Demo\DemoController;
+use App\Http\Controllers\Home\AboutController;
+use App\Http\Controllers\Home\HomeSliderController;
+use App\Http\Controllers\Home\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,17 +19,66 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
+
+Route::controller(DemoController::class)->group(function () {
+    Route::get('/about', 'index')->name('about.page')->middleware('check');
+    Route::get('/contact', 'contact')->name('contact.page')->middleware('contact');
+});
+
+
+//Admin All Route
+Route::controller(AdminController::class)->group(function () {
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
+    Route::get('/admin/profile', 'profile')->name('admin.profile');
+    Route::get('/edit/profile', 'editProfile')->name('edit.profile');
+    Route::post('/store/profile', 'storeProfile')->name('store.profile');
+
+    Route::get('/change/password', 'changePassword')->name('change.password');
+    Route::post('/update/password', 'updatePassword')->name('update.password');
+});
+
+//Home Slide All Route
+Route::controller(HomeSliderController::class)->group(function () {
+    Route::get('/home/slide', 'homeSlider')->name('home.slide');
+    Route::post('/update/slider', 'updateSlider')->name('update.slider');
+    
+});
+
+//About Page All Route
+Route::controller(AboutController::class)->group(function () {
+    Route::get('/about/page', 'aboutPage')->name('about.page');
+    Route::post('/update/about', 'updateAbout')->name('update.about');
+    Route::get('/about', 'homeAbout')->name('home.about');
+    Route::get('/about/multi/image', 'aboutMultiImage')->name('about.multi.image');
+    Route::post('/store/multi/image', 'storeMultiImage')->name('store.multi.image');
+    Route::get('/all/multi/image', 'allMultiImage')->name('all.multi.image');
+    Route::get('/edit/multi/image/{id}', 'editMultiImage')->name('edit.multi.image');
+    Route::post('/update/multi/image', 'updateMultiImage')->name('update.multi.image');
+    Route::get('/delete/multi/image/{id}', 'deleteMultiImage')->name('delete.multi.image');
+    
+});
+
+
+
+//Portfolio All Route
+Route::controller(PortfolioController::class)->group(function () {
+    Route::get('/all/portfolio', 'allPortfolio')->name('all.portfolio');
+    Route::get('/add/portfolio', 'addPortfolio')->name('add.portfolio');
+    Route::post('/store/portfolio', 'storePortfolio')->name('store.portfolio');
+    Route::get('/edit/portfolio/{id}', 'editPortfolio')->name('edit.portfolio');
+    Route::post('/update/portfolio', 'updatePortfolio')->name('update.portfolio');
+    Route::get('/delete/portfolio/{id}', 'deletePortfolio')->name('delete.portfolio');
+    Route::get('/delete/details/{id}', 'portfolioDetails')->name('portfolio.details');
+});
+
+
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
 
 require __DIR__.'/auth.php';
